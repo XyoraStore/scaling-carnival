536c233d20feeb5fdffff50113f2d912a3d1c400
XyoraStore <muhammadatsair10@gmail.com>
2022-02-24 22:05:16 +0800

    tessbang

---
+++ message/donasi.js
@@ -0,0 +1,28 @@
+exports.dodo = (prefix) => {
+return`
+*╭─▸*「 *DONASI* 」
+*┴*
+*│* Bantu Developer ngembangin bot ini
+*│* dengan donasi seiklasnya 🙂
+*│*
+*│* Shopee : 082393072006
+*│* Gopay : 081332368569
+*│* Dana : Gada
+*│* Ovo : 082393072006
+*│*
+*│* Makasih yang udah donasi
+*│* dan bantu ngembangin bot ini
+*│* moga rejekinya lancar amin
+*┬*
+*╰────────────[*`
+}
+
+
+
+
+
+
+
+
+
+
